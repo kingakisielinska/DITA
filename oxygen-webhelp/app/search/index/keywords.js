c1180427a2ffd6da029f322d55b83703546b2570
kingakisielinska <kisielinska.kingaa@gmail.com>
2021-03-25 19:50:17 +0100

    update

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,17 @@
+define(function() {var keywords=[{w:"English",p:["p0"]},{w:"translation",p:["p0","p5"]},{w:"History",p:["p1"]},{w:"Lorem",p:["p2","p3","p4","p8"]},{w:"Ipsum",p:["p2","p4"]},{w:"Ipsum:",p:["p3"]},{w:"original",p:["p3"]},{w:"version",p:["p3"]},{w:"Plugins",p:["p4"]},{w:"Polish",p:["p5"]},{w:"Usage",p:["p6"]},{w:"Variants",p:["p7"]},{w:"and",p:["p7"]},{w:"technical",p:["p7"]},{w:"information",p:["p7"]},{w:"Why",p:["p8"]},{w:"Ipsum?",p:["p8"]},{w:"References",p:["p9"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2];
+ph["p2"]=[3, 4];
+ph["p3"]=[3, 5, 6, 7];
+ph["p4"]=[3, 4, 8];
+ph["p5"]=[9, 1];
+ph["p6"]=[10];
+ph["p7"]=[11, 12, 13, 14];
+ph["p8"]=[15, 3, 16];
+ph["p9"]=[17];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
